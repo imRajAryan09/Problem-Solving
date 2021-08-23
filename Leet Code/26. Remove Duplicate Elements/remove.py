19a98d7c24aa84f12f2877fc34bf5942c5958015
Raj Aryan <81006469+imRajAryan09@users.noreply.github.com>
2021-08-23 12:59:43 +0530

    Add files via upload

---
+++ Leet Code/26. Remove Duplicate Elements/remove.py
@@ -0,0 +1,29 @@
+class Solution:
+    def removeDuplicates(self, nums) -> int:
+        
+        n=len(nums)
+        if n == 0 or n == 1:
+            return n
+
+        # To store index of next
+        # unique element
+        j = 0
+
+        # Doing same as done
+        # in Method 1 Just
+        # maintaining another 
+        # updated index i.e. j
+        for i in range(0, n-1):
+            
+            if nums[i] != nums[i+1]:
+                nums[j] = nums[i]
+                j += 1
+
+        nums[j] = nums[n-1]
+        j += 1
+        return j
+        
+    
+
+sol=Solution()
+print(sol.removeDuplicates([1,1,2]))
