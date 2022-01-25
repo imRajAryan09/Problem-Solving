884b65fdb39ff888360508a6acbf5f26b854ad76
Raj Aryan <raj.12020162@lpu.in>
2022-01-25 12:24:15 +0530

    Add files via upload

--- 30. Substring with Concatenation of All Words/findsub.py
+++ 30. Substring with Concatenation of All Words/findsub.py
@@ -1,3 +1,5 @@
+# 30. Substring with Concatenation of All Words [https: // leetcode.com/problems/substring-with-concatenation-of-all-words/]
+from typing import List
 class SubstringWithConcatenationOfAllWords:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         # Resultant list
