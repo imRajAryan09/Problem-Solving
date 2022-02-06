c6333bcb390f1ed7f8f30009fe0229a92373f473
Raj Aryan <raj.12020162@lpu.in>
2022-02-06 21:50:42 +0530

    Update anagrams.py

--- 438. Find All Anagrams in a String/anagrams.py
+++ 438. Find All Anagrams in a String/anagrams.py
@@ -4,9 +4,13 @@ from typing import List
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        # if the length of string is greater than
+        # the length of pattern then no anagrams
+        # are possible 
         if len(p) > len(s):
             return []
         pCount, sCount = {}, {}
+        # mapping from index 0 to len(p)
         for i in range(len(p)):
             pCount[p[i]] = 1+pCount.get(p[i], 0)
             sCount[s[i]] = 1+sCount.get(s[i], 0)
