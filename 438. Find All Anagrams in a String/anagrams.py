da3687c8eff72bde3e78000d1779b498d9f60071
Raj Aryan <raj.12020162@lpu.in>
2022-02-07 22:29:52 +0530

    Update anagrams.py

--- 438. Find All Anagrams in a String/anagrams.py
+++ 438. Find All Anagrams in a String/anagrams.py
@@ -14,6 +14,8 @@ class Solution:
         for i in range(len(p)):
             pCount[p[i]] = 1+pCount.get(p[i], 0)
             sCount[s[i]] = 1+sCount.get(s[i], 0)
+        # if the chars in string map to the chars
+        # in pattern then they are anagrams
         res = [0] if sCount == pCount else []
         l = 0
         for r in range(len(p), len(s)):
